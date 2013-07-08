85ba208a1e5c884125713a1c3af411ef82793879
Luis Valenzuela <li.valenzuelaa@gmail.com>
2013-07-08 18:14:50 -0400

    fix opciones de agregar productos

--- app/helpers/computers_helper.rb
+++ app/helpers/computers_helper.rb
@@ -3,9 +3,9 @@ module ComputersHelper
 	def select_options(type)
 		case type
 		when 'boolean'
-			return [["No Especificado",""],["Si",1], ["No",0]]
+			return [["Si",1], ["No",0], ["No Especificado",]]
 		when 'numbers'
-			return [["No Especificado",""]["0",0],["1",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9],["10",10],
+			return [["No Especificado",0],["1",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9],["10",10],
     ["11",11],["12",12],["13",13],["14",14],["15",15],["16",16]]
 		end
 	end
