766dbbb4fd148c850fc17a9c8e458bf14b071a08
Luis Valenzuela <li.valenzuelaa@gmail.com>
2013-07-10 12:25:42 -0400

    modificacion a computer.rb

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,6 +1,6 @@
 class Computer < ActiveRecord::Base
-  attr_accessible :img_ref, :marca, :modelo, :categoria, :sist_operativo, :precio, :bateria, :bateria_celdas, :bateria_duracion
-  attr_accessible :proce_marca, :proce_version, :proce_velocidad, :proce_nucleos
+  attr_accessible :img_ref, :marca_id, :modelo, :categoria, :sist_operativo, :precio, :bateria, :bateria_celdas, :bateria_duracion
+  attr_accessible :proce_marca_id, :proce_version, :proce_velocidad, :proce_nucleos
   attr_accessible :memoria, :memoria_tipo, :memoria_velocidad, :memoria_max, :memoria_ranuras
   attr_accessible :disco_capacidad, :disco_velocidad, :disco_ssd, :screen_size, :screen_resol, :screen_type
   attr_accessible :lector_blue_ray, :grabador_blue_ray, :lector_dvd, :grabador_dvd
